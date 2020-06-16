e1ecd278f8ddf702e732b3fbc331b0fb0bfd6e12
Thomas Kienlen <kommander@laposte.net>
2020-06-16 21:59:12 +0200

    Fix docker for ci

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+# REGISTRY_PROJECT_URL ?= my-awesome-registry.org/my-cool-project
+# BUILD_ID ?= commit_sha
+# REF_ID ?= branch_name
+
 all: help
 include *.mk
+
+ci-build: docker-pull docker-build
+ci-push: docker-push
+ci-push-release: docker-push-release
 
 start: docker-compose-pull docker-compose-start ##- Start
 deploy: docker-compose-pull docker-compose-deploy ##- Deploy (start remotely)
--- docker.mk
+++ docker.mk
@@ -1,11 +1,15 @@
-REGISTRY ?= localhost
-BUILD_ID ?= latest
-BUILD_SUBPATH ?= build
-PROJECT_PATH ?= $(notdir $(realpath $(dir $(lastword $(MAKEFILE_LIST)))))
-REGISTRY_PROJECT_URL=$(REGISTRY)/$(PROJECT_PATH)
-CONTAINER_IMAGE ?= $(REGISTRY_PROJECT_URL)/${BUILD_SUBPATH}:$(BUILD_ID)
-CONTAINER_RELEASE_IMAGE=${REGISTRY_PROJECT_URL}:${BUILD_ID}
-CONTAINER_LATEST_IMAGE=${REGISTRY_PROJECT_URL}:latest
+REGISTRY      ?= localhost
+BUILD_ID      ?= edge
+REF_ID        ?= latest
+BUILD_SUBPATH ?= dev
+PROJECT_PATH  ?= $(notdir $(realpath $(dir $(lastword $(MAKEFILE_LIST)))))
+
+REGISTRY_PROJECT_URL ?= $(REGISTRY)/$(PROJECT_PATH)
+
+CONTAINER_BUILD_IMAGE=$(REGISTRY_PROJECT_URL)/${BUILD_SUBPATH}:$(BUILD_ID)
+CONTAINER_REF_IMAGE=$(REGISTRY_PROJECT_URL)/${BUILD_SUBPATH}:$(REF_ID)
+CONTAINER_RELEASE_IMAGE=${REGISTRY_PROJECT_URL}:${REF_ID}
+
 DOCKER_TARGETS=$(shell cat Dockerfile | awk '/^[[:blank:]]*FROM/ { print $$4 }' | xargs)
 
 ## Pull
@@ -13,13 +17,13 @@ DOCKER_TARGETS=$(shell cat Dockerfile | awk '/^[[:blank:]]*FROM/ { print $$4 }' 
 docker-pull-stages: ##- Pull intermediate containers
 	@for docker_target in ${DOCKER_TARGETS}; \
 	do \
-		docker_intermediate_image="${CONTAINER_IMAGE}-$$docker_target"; \
+		docker_intermediate_image="${CONTAINER_BUILD_IMAGE}-$$docker_target"; \
 		docker pull $$docker_intermediate_image || true; \
 	done
 
 .PHONY: docker-pull-final
 docker-pull-final: ##- Pull final container
-	-docker pull $(CONTAINER_IMAGE)
+	-docker pull $(CONTAINER_BUILD_IMAGE)
 
 .PHONY: docker-pull
 docker-pull: docker-pull-stages docker-pull-final ##- Pull containers
@@ -29,41 +33,47 @@ docker-pull: docker-pull-stages docker-pull-final ##- Pull containers
 docker-build-stages: docker-pull-stages ##- Build intermediate containers
 	@for docker_target in ${DOCKER_TARGETS}; \
 	do \
-		docker_intermediate_image="${CONTAINER_IMAGE}-$$docker_target"; \
+		docker_intermediate_image="${CONTAINER_BUILD_IMAGE}-$$docker_target"; \
 		docker build -t $$docker_intermediate_image --target $$docker_target .; \
 	done
 
 .PHONY: docker-build-final
 docker-build-final: docker-pull-final ##- Build final container
-	-docker pull $(CONTAINER_IMAGE)
-	docker build -t $(CONTAINER_IMAGE) .
+	-docker pull $(CONTAINER_BUILD_IMAGE)
+	docker build -t $(CONTAINER_BUILD_IMAGE) .
 
 .PHONY: docker-build
 docker-build: docker-build-stages docker-build-final ##- Build containers
 
 ## Tag
-.PHONY: docker-tag-releases
-docker-tag-releases:
-	docker tag ${CONTAINER_IMAGE} ${CONTAINER_RELEASE_IMAGE}
-	docker tag ${CONTAINER_IMAGE} ${CONTAINER_LATEST_IMAGE}
+.PHONY: docker-tag-ref
+docker-tag-ref:
+	docker tag ${CONTAINER_BUILD_IMAGE} ${CONTAINER_REF_IMAGE}
+
+.PHONY: docker-tag-release
+docker-tag-release: docker-pull-final
+	docker tag ${CONTAINER_BUILD_IMAGE} ${CONTAINER_RELEASE_IMAGE}
 
 ## Push
 .PHONY: docker-push-stages
 docker-push-stages: ##- Push intermediate containers to registry
 	@for docker_target in ${DOCKER_TARGETS}; \
 	do \
-		docker_intermediate_image="${CONTAINER_IMAGE}-$$docker_target"; \
+		docker_intermediate_image="${CONTAINER_BUILD_IMAGE}-$$docker_target"; \
 		docker push $$docker_intermediate_image || true; \
 	done
 
 .PHONY: docker-push-final
 docker-push-final: ##- Push final container to registry
-	docker push $(CONTAINER_IMAGE)
+	docker push $(CONTAINER_BUILD_IMAGE)
 
-.PHONY: docker-push-releases
-docker-push-releases: docker-tag-releases ##- Push releases containers to registry
+.PHONY: docker-push-ref
+docker-push-ref: docker-tag-ref ##- Push ref container to registry
+	docker push ${CONTAINER_REF_IMAGE}
+
+.PHONY: docker-push-release
+docker-push-release: docker-tag-release ##- Push releases containers to registry
 	docker push ${CONTAINER_RELEASE_IMAGE}
-	docker push ${CONTAINER_LATEST_IMAGE}
 
 .PHONY: docker-push
-docker-push: docker-push-stages docker-push-final ##- Push containers to registry
+docker-push: docker-push-stages docker-push-final docker-push-ref ##- Push containers to registry
